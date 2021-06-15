83f1f84aa33b03e777a774eb24bb0319d3e3828a
quan <jiahao.quan@me.com>
2021-06-15 15:31:11 -0400

    redis demo

---
+++ src/test/java/com/quan/redisdemo/RedisdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quan.redisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
